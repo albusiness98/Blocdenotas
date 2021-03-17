0bb8a261bce03b75db8ab26b8628ced4c006d246
albusiness98 <72590054+albusiness98@users.noreply.github.com>
2021-03-16 20:50:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,59 @@
-<?php
+<html>
+
+<head>
+
+</head>
+
+<body>
+
+<form action="index.php" method="post">
+
+Asunto:<br><input type="text" name="asunto"><br><br>
+
+Descripcion: <br><textarea name="descripcion" rows="10" cols="30"></textarea>
+
+<input type="submit" name="boton" value="Crear Archivo">
+
+  
+
+
+
 
 include_once("home.html");
 
+$ar=fopen("archivo.txt","a") or die ("Error al crear");
 
+$asu=$_REQUEST['asunto'];
 
+$des=$_REQUEST['descripcion'];
 
+fwrite($ar, $asu);
 
+fwrite($ar, "\n");
 
+fwrite($ar, $des);
 
-
-
+fwrite($ar, "\n");
 
 
 
 ?>
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
