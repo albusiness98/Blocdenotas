0a050f3ee2e042fd869df44ca7415b10a979a473
albusiness98 <72590054+albusiness98@users.noreply.github.com>
2021-03-16 20:14:05 -0400

    Add files via upload

---
+++ creararchivo.php
@@ -0,0 +1,14 @@
+<?php
+
+$ar=fopen("archivo.txt","a") or die ("Error al crear");
+
+$asu=$_REQUEST['asunto'];
+$des=$_REQUEST['descripcion'];
+
+fwrite($ar, $asu);
+fwrite($ar, "\n");
+fwrite($ar, $des);
+fwrite($ar, "\n");
+echo "Se creo correctamente el archivo de texto";
+
+?>
