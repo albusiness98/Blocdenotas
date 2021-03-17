27dd578c4abc829273ee2caf29b97e058bd5993c
albusiness98 <72590054+albusiness98@users.noreply.github.com>
2021-03-16 20:41:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
 include_once("home.html");
 
-$ar=fopen("archivo.txt","a") or die ("Error al crear");
 
-$asu=$_REQUEST['asunto'];
-$des=$_REQUEST['descripcion'];
 
-fwrite($ar, $asu);
-fwrite($ar, "\n");
-fwrite($ar, $des);
-fwrite($ar, "\n");
-echo "Se creo correctamente el archivo de texto";
+
+
+
+
+
+
+
+
 
 ?>
